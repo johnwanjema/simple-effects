2a6a7c8c14292ebd1dbb55321d4537b94180e1ce
salehudeen <salehudeensaleh@gmail.com>
2019-04-30 10:04:35 +0200

    vfrtgbyhunjimk

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("img").show();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
